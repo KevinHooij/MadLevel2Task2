0e1e3c5f8b6e0d39bc13247627b23daf47971600
KevinHooij <kevin.hooijschuur@hva.nl>
2020-09-29 19:41:41 +0200

    Finished Level 2 Task 2

--- app/src/main/java/com/example/madlevel2task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2task2/MainActivity.kt
@@ -2,11 +2,10 @@ package com.example.madlevel2task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import androidx.recyclerview.widget.*
 import com.example.madlevel2task2.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,7 +29,41 @@ class MainActivity : AppCompatActivity() {
         for (i in Question.QUESTION_LIST.indices) {
             questions.add(Question(Question.QUESTION_LIST[i], Question.ANSWER_LIST[i]))
         }
+
+        createItemTouchHelper().attachToRecyclerView(rvQuestions)
     }
+
+    private fun createItemTouchHelper(): ItemTouchHelper {
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val swiped: Question = questions[position]
+
+                if (direction == ItemTouchHelper.RIGHT && swiped.answerValue ||
+                    direction == ItemTouchHelper.LEFT && !swiped.answerValue
+                ) {
+                    questions.removeAt(position)
+                } else {
+                    Snackbar.make(rvQuestions, "You answered wrong!", Snackbar.LENGTH_SHORT).show()
+
+                }
+                questionAdapter.notifyDataSetChanged()
+            }
+        }
+        return ItemTouchHelper(callback)
+    }
+
+
+
 
 
 }
