805b57c1defe37a216beb31bc2f50d723f7fd79b
KevinHooij <kevin.hooijschuur@hva.nl>
2020-09-29 17:20:50 +0200

    Adds everything except for the ability to swipe for true or false

--- app/src/main/java/com/example/madlevel2task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2task2/MainActivity.kt
@@ -2,16 +2,34 @@ package com.example.madlevel2task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.madlevel2task2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val questions = arrayListOf<Question>()
+    private val questionAdapter = QuestionAdapter(questions)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        initViews()
+    }
+
+    private fun initViews(){
+        binding.rvQuestions.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+        binding.rvQuestions.adapter = questionAdapter
+        binding.rvQuestions.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
+
+        for (i in Question.QUESTION_LIST.indices) {
+            questions.add(Question(Question.QUESTION_LIST[i], Question.ANSWER_LIST[i]))
+        }
     }
 
 
